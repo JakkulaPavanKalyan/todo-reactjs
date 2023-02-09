3c7ad41de56bc80caec03aed63dfe5954f02c3dc
JakkulaPavanKalyan <jakkulapavankalyan02@gmail.com>
2023-02-09 06:49:56 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Todo from './components/Todo'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      
+    <center>
+    
+    <h1
+    style={{backgroundColor: "green",backgroundImage:`url("https://plus.unsplash.com/premium_photo-1661963783275-dff88a0296f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dG8lMjBkbyUyMGxpc3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60")`,backgroundSize:"cover"}}>Make your Todo and Diary</h1>
+
+   </center>
+   
+  <Todo/>
+   
+    
+    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
